efd7bb1012cb29bede47d421658de32a04e17d76
eholgersson <emanuel.holgersson@gmail.com>
2021-04-14 08:39:58 +0200

    Better flow from fetching data for specific team, league and year

--- football_statistics.py
+++ football_statistics.py
@@ -6,25 +6,42 @@ import requests
 import json
 import api_config as cfg
 import os
+import time
+from tqdm import tqdm
+
 print(os.getcwd())
+
+global url, api_key
+
+url = cfg.api_config['url']
+api_key = cfg.api_config['api_key']
+
+# %%
+league = {"id": 39, "name": "Premier League"}
+l = "".join(league['name'].split(" "))
+l
+
+print(api_key)
 
 def config():
     global headers
-    api_key = cfg.api_config['key']
+    api_key = cfg.api_config['api_key']
     headers = {
         "x-apisports-key" : api_key
     }
 
-def get_response():
-    url = "https://v3.football.api-sports.io/teams/statistics"
+# get teams from league and year
+def get_response(league, year):
+    url = f"https://v3.football.api-sports.io/teams?league={league}&season={year}"
     payload={}
-    api_key = cfg.api_key['api_key']
+    api_key = cfg.api_config['api_key']
     headers = {
         "x-apisports-key" : api_key
     }
 
     resp = requests.get(url=url, headers=headers, data=payload)
     print(resp)
+
     return resp
 
 def write_to_json(resp, filename):
@@ -55,43 +72,96 @@ def save_to_csv(df, filename):
     print(os.getcwd())
     print("saved to: " + filename)
 
-    
+
+# get team statistics
 def get_team_statistics(team, league, year):
     config()
     
     url = cfg.api_config['url']
-    api_key = cfg.api_config['api_key']
-    
-    resp = requests.get(url, headers=headers)
+
+    url += f"teams/statistics?season={year}&team={team}&league={league}"
+    #print('in config: ', headers)
+    #print(url)
+    payload={}
+
+    resp = requests.get(url, headers=headers, data=payload)
 
     resp = resp.json()
 
-    # team statistic: json to df
+    #print(resp)
+
+    try:
+        # team statistic: json to df
+        team = resp['response']['team']
+        fixtures = resp['response']['fixtures']
+        df_team = pd.json_normalize(team)
+        df_fixtures = pd.json_normalize(fixtures)
+        df = pd.json_normalize(resp['response'])
+        #print(df.columns)
+        #print(df_team.head())
+        #print(df_fixtures.head())
+
+        # Adds fixtures and team for the specified team
+        df = pd.concat([df_fixtures,df_team], axis=1)
+    except TypeError:
+        print(resp)
     
+
+    return df
+        
+
+def main_statistics(league_id, year):
+    # from api
+    resp = get_response(league_id, year)
+    resp = resp.json()
+
     df = pd.json_normalize(resp['response'])
     print(df.head())
-    
-    return df
+    print(df.columns)
+
+    # team to loop through via team id
+    team_id = df['team.id']
+    columns = ["played.home", "played.away", "played.total", "wins.home", "wins.away", "wins.total", "draws.home", "draws.away", "draws.total", "loses.home", "loses.away" ,"loses.total","id","name","logo"]
+    df_stats = pd.DataFrame(columns=columns)
+    excluded = [10]
+    for t_id in tqdm(team_id):
+        if t_id in excluded:
+            continue
+        df = get_team_statistics(t_id,league_id, year)
+        df_stats = df_stats.append(df, ignore_index=True)
+        time.sleep(6.1)
+    print(df_stats.head())
+
+    l = "".join(league['name'].split(" "))
+    filename= f"teams_statistics_{l}_{year}.csv"
+    save_to_csv(df_stats, filename)
+
+
 
 
 def main():
     
-    # from api
-    resp = get_response()
-    
-    resp = resp.json()
 
-    print(os.getcwd())
-    #save response
-    version = 1
-    filename = '/Users/emanuel.holgersson/Documents/Python/REST_APIs/football_statistics_teams_statistics'+str(version)+'.json'
-    write_to_json(resp, filename)
+    # get teams and team statistics for a specific league and year 
+    main_statistics(league['id'], 2019)
+
+
+    if 0:
+
+        print(os.getcwd())
+        #save response
+        version = 4
+        filename = '/Users/emanuel.holgersson/Documents/Python/REST_APIs/football_statistics_teams_'+str(version)+'.json'
+        
+        #write_to_json(resp, filename)
+
+    #go_through(filename)
 
     # from file
     #resp = load_json(filename)
     
 
-    df = teams_endpoint(resp)
+    # df = teams_endpoint(resp)
 
     #save_to_csv(df, filename)
 
@@ -116,3 +186,24 @@ def main():
 if __name__ == '__main__':
     main()
 
+
+# %%
+import pandas
+import json
+
+people = {1: {'name': 'John', 'age': '27', 'sex': 'Male'}}
+
+print(people[1]['name'])
+
+# %%
+
+import pandas as pd
+
+df1 = pd.DataFrame([1,2,3], columns=['a'])
+df1
+df2 = pd.DataFrame([1,2,3], columns=['b'])
+df2
+
+# %%
+pd.concat([df1, df2], axis=1)
+# %%
